2a3f5b6fd5f91ca22e64a4cee4968c5411b3f6da
drew <drew@drew.pro>
2018-07-18 15:31:31 +0100

    InterestByFee removed

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,5 @@
 const TimeLocked = artifacts.require("TimeLocked");
-const InterestByFee = artifacts.require("InterestByFee");
 
 module.exports = function(deployer) {
     deployer.deploy(TimeLocked);
-    deployer.deploy(InterestByFee);
 };
